472bb887bec20d7c68539ee88f6283b895fc71b5
PabloAranda00 <pabloarandalba1b@gmail.com>
2023-12-01 19:44:08 +0100

    Es el examen

---
+++ ProyectoExEj2/src/Polvoron/Polvoron.java
@@ -0,0 +1,10 @@
+package Polvoron;
+
+public class Polvoron {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Soy Pablo y me encantan los polvorones");
+	}
+
+}
